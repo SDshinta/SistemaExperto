ade32a3a703666e3178ea8062a7bee612672ec3f
SDshinta <138565003+SDshinta@users.noreply.github.com>
2025-11-01 16:03:41 -0600

    Update models.py

--- taller/models.py
+++ taller/models.py
@@ -9,10 +9,11 @@ class Hecho(models.Model):
 
 
 class Regla(models.Model):
-    hecho = models.CharField(max_length=100, null=True, blank=True)    # condición única (ej: "no_enciende")
+    hecho = models.CharField(max_length=100, blank=True, blank=True)    # condición única (ej: "no_enciende")
     conclusion = models.CharField(max_length=100)  # ej: "Fallo en fuente de poder"
     peso = models.IntegerField(default=1)
     explicacion_humana = models.TextField(blank=True, null=True, help_text="Explicación amigable para el usuario")
 
     def __str__(self):
         return f"SI {self.hecho} ENTONCES {self.conclusion} (Peso: {self.peso})"
+
