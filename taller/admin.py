729b1c4db05dee07032c993f470b5ecd044f991a
SDshinta <138565003+SDshinta@users.noreply.github.com>
2025-11-01 15:25:45 -0600

    Update admin.py

--- taller/admin.py
+++ taller/admin.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
-
-# Register your models here.
-
-from django.contrib import admin
 from .models import Hecho, Regla
 
 admin.site.register(Hecho)
+
 admin.site.register(Regla)
